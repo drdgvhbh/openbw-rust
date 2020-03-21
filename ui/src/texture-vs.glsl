dfdf64fd2fdc67aa3cd3bd7f54e3836ebbe1e835
Ryan Lee <drdgvhbh@gmail.com>
2020-03-21 16:13:35 -0400

    Excellent work. Now your trip to quel'thalas can begin

--- ui/src/texture-vs.glsl
+++ ui/src/texture-vs.glsl
@@ -3,6 +3,6 @@ in vec2 position;
 out vec2 v_uv;
 
 void main() {
-  gl_Position = vec4(position * 3, 0., 1.);
+  gl_Position = vec4(position * 5, 0, 1.);
   v_uv = position * .5 + .5; // transform the position of the vertex into UV space
 }
